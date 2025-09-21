a5fc99fa3376fb1d55b48119d7b35e8a52d41782
Stefan Deronjic <stefan86d@gmail.com>
2025-09-21 18:07:41 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/components/layout/sidebar-nav.tsx
+++ src/components/layout/sidebar-nav.tsx
@@ -34,7 +34,7 @@ export function SidebarNav({ userRole }: SidebarNavProps) {
     return (
         <nav className="grid items-start text-sm font-medium">
             {navItems.map(({ href, icon: Icon, label }) => (
-                <Link key={href} href={href} legacyBehavior passHref>
+                <Link key={href} href={href}>
                     <DropdownMenuItem
                         className={cn(
                             "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary",
