6225205244ead674831fce40f8cfb5ba22ca3177
Stefan Deronjic <stefan86d@gmail.com>
2025-09-22 10:59:18 +0000

    make the black theme again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body className={`${inter.variable} font-body antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="dark"
           disableTransitionOnChange
         >
             <UserProvider>
