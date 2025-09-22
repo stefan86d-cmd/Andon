cd23fed3f75601416ed1e5e6e1366e0596fa99fa
Stefan Deronjic <stefan86d@gmail.com>
2025-09-22 11:06:49 +0000

    on the left top drop down menu, selected category is black not blue

--- src/components/layout/sidebar-nav.tsx
+++ src/components/layout/sidebar-nav.tsx
@@ -38,7 +38,7 @@ export function SidebarNav({ userRole }: SidebarNavProps) {
                     <DropdownMenuItem
                         className={cn(
                             "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary",
-                            pathname === href && "bg-muted text-primary"
+                            pathname === href && "bg-black text-white hover:bg-black/90 hover:text-white"
                         )}
                     >
                         <Icon className="h-4 w-4" />
