1806de4bad9fe059fca66e996b46bc381de89cb6
Stefan Deronjic <stefan86d@gmail.com>
2025-09-23 20:13:50 +0000

    you broke the page because you added your own logo for dark theme even t

--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -3,25 +3,24 @@
 
 import Image from 'next/image';
 import { cn } from '@/lib/utils';
-import { useTheme } from 'next-themes';
-import { useEffect, useState } from 'react';
 
 export function Logo({ className }: { className?: string }) {
-    const { resolvedTheme } = useTheme();
-    const [logoSrc, setLogoSrc] = useState("/Andonpro_Logo_Musta_Syvätty.png");
-
-    useEffect(() => {
-        setLogoSrc(resolvedTheme === 'dark' ? "/Andonpro_Logo_Valkoinen_Syvätty.png" : "/Andonpro_Logo_Musta_Syvätty.png");
-    }, [resolvedTheme]);
-
     return (
       <div className={cn("flex items-center gap-2 font-semibold text-primary", className)}>
           <Image
-            src={logoSrc}
+            src="/Andonpro_Logo_Musta_Syvätty.png"
             alt="AndonPro logo"
             width={100}
             height={24}
-            className="h-auto w-auto"
+            className="h-auto w-auto dark:hidden"
+            priority
+        />
+         <Image
+            src="/Andonpro_Logo_Valkoinen_Syvätty.png"
+            alt="AndonPro logo"
+            width={100}
+            height={24}
+            className="h-auto w-auto hidden dark:block"
             priority
         />
       </div>
