852f6e8beeef9fcd562bcc2ad1e603cb219d9751
Stefan Deronjic <stefan86d@gmail.com>
2025-09-25 11:39:23 +0000

    File changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/toaster"
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from '@/components/layout/theme-provider';
 import { UserProvider } from '@/contexts/user-context';
+import { FirebaseClientProvider } from '@/firebase';
 
 export const metadata: Metadata = {
   title: 'AndonPro',
@@ -28,10 +29,12 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <FirebaseClientProvider>
             <UserProvider>
                 {children}
             </UserProvider>
             <Toaster />
+          </FirebaseClientProvider>
         </ThemeProvider>
       </body>
     </html>
