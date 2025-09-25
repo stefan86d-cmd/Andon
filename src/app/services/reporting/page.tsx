f2060b97c7998e01871133d7ab2f1cde880389cf
Stefan Deronjic <stefan86d@gmail.com>
2025-09-25 10:57:40 +0000

    Replace Reports3.png with Reports4.png

--- src/app/services/reporting/page.tsx
+++ src/app/services/reporting/page.tsx
@@ -79,7 +79,7 @@ export default function AnalyticsReportingPage() {
 
         <section className="container text-center pb-20">
           <Image
-            src="/Reports3.png"
+            src="/Reports4.png"
             alt="Analytics & Reporting Screenshot"
             width={1200}
             height={675}
