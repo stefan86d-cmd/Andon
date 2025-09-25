52af71e2e984c9a556e654c66748af68fec132b9
Stefan Deronjic <stefan86d@gmail.com>
2025-09-25 11:38:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5908745554-37561",
+  "appId": "1:122772761857:web:9ed040003bdcd05eae3f00",
+  "apiKey": "AIzaSyB2GWsEfRVVhwVl-arUo6Px26jLb4WKMQg",
+  "authDomain": "studio-5908745554-37561.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "122772761857"
+};
