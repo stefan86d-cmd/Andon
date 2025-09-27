12fdd45b935855837224f65be57bfb72f361ebce
Stefan Deronjic <stefan86d@gmail.com>
2025-09-27 11:58:25 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  "projectId": "studio-5908745554-37561",
-  "appId": "1:122772761857:web:9ed040003bdcd05eae3f00",
-  "apiKey": "AIzaSyB2GWsEfRVVhwVl-arUo6Px26jLb4WKMQg",
-  "authDomain": "studio-5908745554-37561.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "122772761857"
+  apiKey: "AIzaSyB2GWsEfRVVhwVl-arUo6Px26jLb4WKMQg",
+  authDomain: "studio-5908745554-37561.firebaseapp.com",
+  projectId: "studio-5908745554-37561",
+  storageBucket: "studio-5908745554-37561.firebasestorage.app",
+  messagingSenderId: "122772761857",
+  appId: "1:122772761857:web:9ed040003bdcd05eae3f00"
 };
 
 // Initialize Firebase
