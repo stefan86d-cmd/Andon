759c099d2c0c5f0f5a4502b9f21cd7edba066afb
Stefan Deronjic <stefan86d@gmail.com>
2025-10-03 08:16:16 +0000

    i did it in google console retry build, but still the same error

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 
 import type {NextConfig} from 'next';
-import withPWA from '@ducanh2912/next-pwa';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -40,16 +39,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-const pwaConfig = withPWA({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-});
-
-export default pwaConfig(nextConfig);
+export default nextConfig;
