c5adaa96ece3c2d80f37336f1e69c85734fbae79
Stefan Deronjic <stefan86d@gmail.com>
2025-10-04 19:05:43 +0000

    can you check every file in studio we had made, inspect them, see if the

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "placehold.co", pathname: "/**" },
       { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
       { protocol: "https", hostname: "picsum.photos", pathname: "/**" },
-      { protocol: "httpshttps://lh3.googleusercontent.com", hostname: "lh3.googleusercontent.com", pathname: "/**" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com", pathname: "/**" },
     ],
   },
 };
