2eb6c6c0804bf9bb2d6f246555ea0c6d0267048e
Stefan Deronjic <stefan86d@gmail.com>
2025-10-18 17:50:58 +0000

    can we make our andonpro logo in top bar a bit larger?

--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -10,16 +10,16 @@ export function Logo({ className }: { className?: string }) {
           <Image
             src="/Andonpro_Logo_Musta_Syvätty.png"
             alt="AndonPro logo"
-            width={100}
-            height={24}
+            width={120}
+            height={29}
             priority
             className="dark:hidden"
         />
          <Image
             src="/Andonpro_Logo_Valkoinen_Syvätty.png"
             alt="AndonPro logo"
-            width={100}
-            height={24}
+            width={120}
+            height={29}
             className="hidden dark:block"
             priority
         />
